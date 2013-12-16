641e6d91d7f3c0a47f6c84120c87013bbae71088
Felipe Sales <sales.felipeb@gmail.com>
2013-12-16 17:43:22 -0300

    mopa ajeitada

--- lab3/app/controllers/Application.java
+++ lab3/app/controllers/Application.java
@@ -6,12 +6,10 @@ import play.mvc.*;
 public class Application extends Controller {
 	
 	//Sistema de Planejamento de Disciplinas
-<<<<<<< HEAD
-	
+
 	//CONTROLER: SPD eh a classe responsavel por controlar o meu sistema
-=======
+	
 	//CREATOR: a classe Application usa a classe sistema
->>>>>>> dd294bdc090b947c9900b10db166b1564351eb20
 	static SPD sistema = new SPD();
 	
 	public static Result index() {
