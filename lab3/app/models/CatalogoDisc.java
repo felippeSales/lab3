a1913794a717ba3ccd1ec1f46391170f73aa69aa
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2013-12-22 16:58:56 -0300

    adicionando novas disciplinas

--- lab3/app/models/CatalogoDisc.java
+++ lab3/app/models/CatalogoDisc.java
@@ -12,6 +12,20 @@ public class CatalogoDisc {
 		todasDisciplinas.add(new Disciplina("Leitura e Produção de Textos", 4));
 		todasDisciplinas.add(new Disciplina("Cálculo I", 4));
 		todasDisciplinas.add(new Disciplina("Álgebra Vetorial e Geometria Analítica", 4));
+		todasDisciplinas.add(new Disciplina("Cálculo Diferencial e Integral II", 4));
+		todasDisciplinas.add(new Disciplina("Matemática Discreta", 4));
+		todasDisciplinas.add(new Disciplina("Programação II", 4));
+		todasDisciplinas.add(new Disciplina("Teoria dos Grafos", 2));
+		todasDisciplinas.add(new Disciplina("Fundamentos de Física Clássica", 4));
+		todasDisciplinas.add(new Disciplina("Lab. de Programação II", 4));
+		todasDisciplinas.add(new Disciplina("Álgebra Linear", 4));
+		todasDisciplinas.add(new Disciplina("Probabilidade e Estatística", 4));
+		todasDisciplinas.add(new Disciplina("Teoria da Computação", 4));
+		todasDisciplinas.add(new Disciplina("Estruturas de Dados e Algoritmos", 4));
+		todasDisciplinas.add(new Disciplina("Fundamentos de Física Moderna", 4));
+		todasDisciplinas.add(new Disciplina("Gerência da Informação", 4));
+		todasDisciplinas.add(new Disciplina("Lab. de Estruturas de Dados e Algoritmos", 4));
+		
 	}
 	
 	public List<Disciplina> getCatalogo(){
