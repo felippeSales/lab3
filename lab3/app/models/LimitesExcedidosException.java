4c91fd6e079a5ff766020eacc6b6b7d2f55ee3d3
Felipe Sales <sales.felipeb@gmail.com>
2014-02-17 23:40:50 -0300

    Refatorando as excessoes

--- lab3/app/models/LimitesExcedidosException.java
+++ lab3/app/models/LimitesExcedidosException.java
@@ -11,7 +11,7 @@ public class LimitesExcedidosException extends Exception{
 	private static final long serialVersionUID = 1L;
 	
 	public LimitesExcedidosException(){
-		super("Limite de créditos , no periodo,  excedido!");
+		super("Limite de créditos, no periodo, excedido!");
 	}
 	
 	/*
